9c25fb31bf394ecb65956ffe97595159544ba372
Yasmim Rosa <mimsayrosa@gmail.com>
2022-03-29 09:06:47 -0300

    config iniciais modal landing page

---
+++ js/landingPage.js
@@ -0,0 +1,9 @@
+//pegando o container
+let modal = document.getElementById("modal");
+
+//pegando o botão que ativa o modal
+let buttonModal = document.getElementById("btn btn-primary");
+
+//pegando o span que vai fechar o modal
+let closeModal = document.getElementsByClassName("close")[0];
+
